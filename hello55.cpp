c26897910563455ff41d5537560dff18591f161a
GodBoyDad <75253734+GodBoyDad@users.noreply.github.com>
2021-03-04 08:00:55 +0600

    Add files via upload

---
+++ hello55.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+string* foo() {
+    string* hello = 0;
+    hello = new string("Hello World!");
+    return hello;
+}
+
+int main()
+{
+    string* hello = foo();
+    cout << *hello << endl;
+
+    delete hello;
+
+    return 0;
+}
